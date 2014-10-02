a406393cf8e724c210503abc4d338816d8a9742c
gregleblanc <gregleblanc227@gmail.com>
2014-10-02 10:39:57 -0400

    transitions

---
+++ js/main.js
@@ -0,0 +1,15 @@
+var sales = $('.sales');
+var sales__details = $('.sales__details');
+
+var buttonClickHandler = function () {
+	var currentState = sales__details.attr('data-state');
+
+	if(currentState == 'active'){
+ sales__details.attr('data-state', 'inactive');
+	} else {
+ sales__details.attr('data-state', 'active');
+	}
+
+};
+
+sales.on('click', buttonClickHandler);
