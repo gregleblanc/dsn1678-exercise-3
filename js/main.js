c37d5f46fb93e6509d6ae18b364078f24052774c
gregleblanc <gregleblanc227@gmail.com>
2014-10-02 10:49:41 -0400

    final

--- js/main.js
+++ js/main.js
@@ -1,15 +1,29 @@
-var sales = $('.sales');
-var sales__details = $('.sales__details');
+var sales1 = $('.sales1');
+var sales2 = $('.sales2');
+var sales__details1 = $('.sales__details1');
+var sales__details2 = $('.sales__details2');
 
-var buttonClickHandler = function () {
-	var currentState = sales__details.attr('data-state');
+var buttonClickHandler1 = function () {
+	var currentState = sales__details1.attr('data-state');
 
 	if(currentState == 'active'){
- sales__details.attr('data-state', 'inactive');
+ sales__details1.attr('data-state', 'inactive');
 	} else {
- sales__details.attr('data-state', 'active');
+ sales__details1.attr('data-state', 'active');
 	}
 
 };
 
-sales.on('click', buttonClickHandler);
+var buttonClickHandler2 = function () {
+	var currentState = sales__details2.attr('data-state');
+
+	if(currentState == 'active'){
+ sales__details2.attr('data-state', 'inactive');
+	} else {
+ sales__details2.attr('data-state', 'active');
+	}
+
+};
+
+sales1.on('click', buttonClickHandler1);
+sales2.on('click', buttonClickHandler2);
